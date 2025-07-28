e16e6ccc4a47eabeb33da59e700b6891868695ae
Isaac <isaacsaado69@gmail.com>
2025-07-28 11:11:01 -0400

    update: rewiews

--- resources/js/Pages/Reviews/Show.vue
+++ resources/js/Pages/Reviews/Show.vue
@@ -77,10 +77,10 @@ const confirmDelete = () => {
                         <Link :href="route('orders.show', order.id)" class="inline-flex items-center px-4 py-2 bg-gray-200 border border-transparent rounded-md font-semibold text-xs text-gray-700 uppercase tracking-widest hover:bg-gray-300 focus:bg-gray-300 active:bg-gray-400 focus:outline-none focus:ring-2 focus:ring-gray-300 focus:ring-offset-2 transition ease-in-out duration-150">
                             Volver a la Orden
                         </Link>
-                        <Link v-if="can.create_reviews" :href="route('reviews.edit', [order.id, review.id])" class="inline-flex items-center px-4 py-2 bg-yellow-500 border border-transparent rounded-md font-semibold text-xs text-white uppercase tracking-widest hover:bg-yellow-600 focus:bg-yellow-600 active:bg-yellow-700 focus:outline-none focus:ring-2 focus:ring-yellow-500 focus:ring-offset-2 transition ease-in-out duration-150">
+                        <Link v-if="can.edit_reviews" :href="route('reviews.edit', [order.id, review.id])" class="inline-flex items-center px-4 py-2 bg-yellow-500 border border-transparent rounded-md font-semibold text-xs text-white uppercase tracking-widest hover:bg-yellow-600 focus:bg-yellow-600 active:bg-yellow-700 focus:outline-none focus:ring-2 focus:ring-yellow-500 focus:ring-offset-2 transition ease-in-out duration-150">
                             Editar Revisión
                         </Link>
-                        <DangerButton v-if="can.create_reviews" @click="confirmDelete()">
+                        <DangerButton v-if="can.delete_reviews" @click="confirmDelete()">
                             Eliminar Revisión
                         </DangerButton>
                     </div>
